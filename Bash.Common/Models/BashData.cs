12889fe687ce7874c7a330e49893b74e2de61bad
bsautermeister <apps@bsautermeister.de>
2014-10-14 10:09:46 +0200

    Improved parser rules

--- Bash.Common/Models/BashData.cs
+++ Bash.Common/Models/BashData.cs
@@ -12,10 +12,7 @@ namespace Bash.Common.Models
     [DataContract]
     public class BashData : ViewModelBase
     {
-        private static readonly string[] NEWLINE_DELEMITERS = { "[newline]<", "[newline] <" };
         private const string NEWLINE = "[newline]";
-
-        private static readonly string[] CUTSOM_SEPERATORS = { "---- 1 Stunde später ----" };
 
         private List<BashQuoteItem> _cachedQuoteItems;
 
@@ -154,26 +151,33 @@ namespace Bash.Common.Models
                 return text.Substring(2, text.Length - 2);
             }
 
+            if (text.StartsWith("*"))
+            {
+                return text.Substring(1, text.Length - 1);
+            }
+
             return text;
         }
 
         private bool IsServerText(string text)
         {
-            return (text.StartsWith("*") && 
-                    (text.Contains("was banned from the server") || 
-                    text.Contains("is back from") || 
-                    text.Contains("was kicked by") || 
+            return ((text.StartsWith("*") || text.StartsWith("<--") || text.StartsWith("-->") || text.StartsWith("-!-")) && 
+                    (text.Contains(" was banned from the server") || 
+                    text.Contains(" is back from") || 
+                    text.Contains(" was kicked by") || 
                     text.Contains("Quits: ") || 
                     text.Contains("Joins: ") || 
-                    text.Contains("has joined") || text.Contains("has quit (") || 
-                    text.Contains("changed nick to") || 
-                    text.Contains("wirft seine Tastatur ausm Fenster") || 
-                    text.Contains("is now known as") ||
+                    text.Contains(" has joined") || 
+                    text.Contains(" has quit (") || 
+                    text.Contains(" changed nick to") || 
+                    text.Contains(" wirft seine Tastatur ausm Fenster") || 
+                    text.Contains(" is now known as") ||
                     text.Contains("Parts: ") ||
-                    text.Contains("sets mode: ") ||
-                    text.Contains("is away -")) ||
-                (text.StartsWith("<--") || (text.StartsWith("-->")) && (text.Contains("has quit (") || text.Contains(") has joined"))) ||
-                text.Contains("has quit IRC") ||
+                    text.Contains(" sets mode: ") ||
+                    text.Contains(" left channel (") ||
+                    text.Contains(" has left ") ||
+                    text.Contains(" is away -")) ||
+                text.Contains(" has quit IRC") ||
                 text.Equals("---- 1 Stunde später ----") ||
                 text.Equals("- etwa einen Tag später -"));
         }
